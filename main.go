56a774f564a6fea0bb70f10b110603cff1026b20
Pim van den Berg <pim.van.den.berg@mendix.com>
2018-01-12 11:59:39 +0100

    add support for reading from files directly
    A file can be a named pipe (mkfifo).

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/juju/ratelimit"
@@ -15,6 +16,7 @@ var (
 	drops    int
 	flagRate int64
 	flagKeep bool
+	flagFile string
 )
 
 func init() {
@@ -24,6 +26,7 @@ func init() {
 	}
 	flag.Int64Var(&flagRate, "r", int64(rate), "limit to r messages per second (drops those exceeding the limit)")
 	flag.BoolVar(&flagKeep, "k", false, "keep the messages instead of dropping them")
+	flag.StringVar(&flagFile, "f", "", "define a file as input")
 	flag.Parse()
 }
 
@@ -39,8 +42,30 @@ func parseMendixLogline(line string) string {
 	return re.ReplaceAllString(line, "")
 }
 
+func openFile(file string) (*os.File, error) {
+	if _, err := os.Stat(file); err != nil {
+		return nil, errors.New(fmt.Sprintf("Failed to open \"%s\" (file does not exist)", file))
+	}
+
+	input, err := os.Open(file)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("Failed to open \"%s\" (%s)", file, err))
+	}
+	return input, nil
+}
+
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	var input *os.File
+	if flagFile != "" {
+		var err error
+		input, err = openFile(flagFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	} else {
+		input = os.Stdin
+	}
+	scanner := bufio.NewScanner(input)
 	c := make(chan string)
 	done := make(chan bool)
 	go func(c chan string, done chan bool) {
